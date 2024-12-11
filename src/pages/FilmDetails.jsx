9c711f71c812db8e9db5d35dedb72df3bd506da3
JacopoTrincanato <trincanatoj@gmail.com>
2024-12-11 13:03:43 +0100

    create reviews const in FilmDetails.jsx

--- src/pages/FilmDetails.jsx
+++ src/pages/FilmDetails.jsx
@@ -1,6 +1,34 @@
 //creo il componente FilmDetails
 export default function FilmDetails() {
 
+    //creo una costante dove salvare le reviews
+    const reviews = [
+        {
+            id: 1,
+            name: "Grace",
+            vote: 5,
+            text: "A heartbreaking love story.",
+            created_at: "2024-11-29T10:40:13.000Z",
+            updated_at: "2024-11-29T10:40:13.000Z"
+        },
+        {
+            id: 2,
+            name: "Hank",
+            vote: 4,
+            text: "Beautiful visuals and a moving plot.",
+            created_at: "2024-11-29T10:40:13.000Z",
+            updated_at: "2024-11-29T10:40:13.000Z"
+        },
+        {
+            id: 3,
+            name: "Ivy",
+            vote: 3,
+            text: "A bit too melodramatic for my taste.",
+            created_at: "2024-11-29T10:40:13.000Z",
+            updated_at: "2024-11-29T10:40:13.000Z"
+        }
+    ];
+
     //eseguo il return
     return (
         <>
