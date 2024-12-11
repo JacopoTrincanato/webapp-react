1df5d636be2e95bb88d60529c6e2045fa05849d7
JacopoTrincanato <trincanatoj@gmail.com>
2024-12-11 17:59:18 +0100

    fix bugs

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -1,16 +1,20 @@
 import { Link } from "react-router-dom";
 
+//creo il componente NotFound
 export default function NotFound() {
 
     //eseguo il return
     return (
         <>
 
-            <div className="container m-auto bg-danger">
+            <div className="container d-flex justify-content-evenly align-items-center m-auto">
 
-                <h1 className="text-center">Error 404! Not Found</h1>
+                <div className="min-vh-100">
+                    <h1 className="text-center">Error 404! Not Found</h1>
 
-                <Link to="/" className="btn btn-warning"> <i className="bi bi-arrow-left"></i> Torna alla Homepage</Link>
+                    <Link to="/" className="btn btn-warning"> <i className="bi bi-arrow-left"></i> Torna alla Homepage</Link>
+                </div>
+
 
             </div>
         </>
