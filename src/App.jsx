d7f4f8c34a7347acaef9968a6c80408b8b0040bd
JacopoTrincanato <trincanatoj@gmail.com>
2024-12-11 12:33:38 +0100

    define routes for layout, homepage and film details page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
-function App() {
+//importo BrowserRouter, Routes e Route da react-router-dom
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
+function App() {
 
   return (
     <>
-
+      <BrowserRouter>
+        <Routes>
+          <Route> {/*Layout */}
+            <Route /> {/*Homepage */}
+            <Route /> {/*Film details page */}
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
