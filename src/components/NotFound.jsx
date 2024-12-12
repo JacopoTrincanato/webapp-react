c1dacc88a638759d28454a1ea977cb20ccd6e508
JacopoTrincanato <trincanatoj@gmail.com>
2024-12-12 16:12:38 +0100

    add 404.webp in public folder

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -7,14 +7,12 @@ export default function NotFound() {
     return (
         <>
 
-            <div className="container d-flex justify-content-evenly align-items-center m-auto">
+            <div className="container d-flex flex-column justify-content-center align-items-center min-vh-100">
 
-                <div className="min-vh-100">
-                    <h1 className="text-center">Error 404! Not Found</h1>
+                <img src="../public/404.webp" className="w-50" alt="" />
+                <h1 className="text-center mt-5">Error 404! Not Found</h1>
 
-                    <Link to="/" className="btn btn-warning"> <i className="bi bi-arrow-left"></i> Torna alla Homepage</Link>
-                </div>
-
+                <Link to="/" className="btn btn-warning mt-4"> <i className="bi bi-arrow-left"></i>Torna alla Homepage</Link>
 
             </div>
         </>
