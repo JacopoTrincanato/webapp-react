6509e2f8eb938ddb4e524af87d4fd6ae2771a971
JacopoTrincanato <trincanatoj@gmail.com>
2024-12-12 12:38:02 +0100

    clean code

--- src/pages/FilmDetailsPage.jsx
+++ src/pages/FilmDetailsPage.jsx
@@ -8,51 +8,8 @@ import ReviewsCard from "../components/ReviewsCard";
 //creo il componente FilmDetailsPage
 export default function FilmDetailsPage() {
 
+    //creo una costante dove salvare l'id del singolo film
     const { id } = useParams()
-
-    //creo una costante dove salvare le reviews
-    /*const reviews = [
-        {
-            id: 1,
-            name: "Grace",
-            vote: 5,
-            text: "A heartbreaking love story.",
-            created_at: "2024-11-29T10:40:13.000Z",
-            updated_at: "2024-11-29T10:40:13.000Z"
-        },
-        {
-            id: 2,
-            name: "Hank",
-            vote: 4,
-            text: "Beautiful visuals and a moving plot.",
-            created_at: "2024-11-29T10:40:13.000Z",
-            updated_at: "2024-11-29T10:40:13.000Z"
-        },
-        {
-            id: 3,
-            name: "Ivy",
-            vote: 3,
-            text: "A bit too melodramatic for my taste.",
-            created_at: "2024-11-29T10:40:13.000Z",
-            updated_at: "2024-11-29T10:40:13.000Z"
-        },
-        {
-            id: 4,
-            name: "Mark",
-            vote: 2,
-            text: "too melodramatic for my taste.",
-            created_at: "2024-11-29T10:40:13.000Z",
-            updated_at: "2024-11-29T10:40:13.000Z"
-        },
-        {
-            id: 5,
-            name: "Anna",
-            vote: 1,
-            text: "I didn't like it",
-            created_at: "2024-11-29T10:40:13.000Z",
-            updated_at: "2024-11-29T10:40:13.000Z"
-        }
-    ];*/
 
     //creo una costante per l'url
     const url = `http://localhost:3005/movies/${id}`;
