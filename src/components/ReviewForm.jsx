6b4753881dd78c8a0cf411b297eba8bbc7b7abe4
JacopoTrincanato <trincanatoj@gmail.com>
2024-12-13 16:57:25 +0100

    add classes to form fields

--- src/components/ReviewForm.jsx
+++ src/components/ReviewForm.jsx
@@ -14,15 +14,6 @@ export default function ReviewForm() {
     //creo una costante dove salvare i dati del form
     const [formData, setFormData] = useState(newReview);
 
-    //creo una costante dove salvare lo username
-    //const [username, setUsername] = useState('');
-
-    //creo una costante dove salvare la recensione
-    //const [review, setReview] = useState('');
-
-    //creo una costante dove aggiungere il proprio voto
-    //const [rating, setRating] = useState(0);
-
     //creo la funzione handleFormData dove aggiornare i dati del form
     function handleFormData(e) {
         setFormData((formData) => ({
@@ -39,26 +30,28 @@ export default function ReviewForm() {
     return (
         <>
             <div className="container">
-                <div className="card">
+                <div className="card mb-4">
                     <div className="card-body">
                         <form onSubmit={handleFormSubmit}>
 
                             {/*input per lo username */}
                             <div className="mb-3">
                                 <label htmlFor="username">Username</label>
-                                <input type="text" placeholder="Inserisci lo username" value={formData.username} onChange={handleFormData} />
+                                <input type="text" placeholder="Inserisci lo username" className="form-control" value={formData.username} onChange={handleFormData} />
                             </div>
 
                             {/*input per la recensione */}
                             <div className="mb3">
                                 <label htmlFor="review">Review</label>
-                                <textarea name="review" id="review" placeholder="Inserisci la recensione" value={formData.review} onChange={handleFormData}></textarea>
+                                <textarea name="review" id="review" placeholder="Inserisci la recensione" className="form-control" value={formData.review} onChange={handleFormData}></textarea>
                             </div>
 
                             {/*imput per il voto*/}
-                            <div className="rating mb-3 text-warning">
-                                {[1, 2, 3, 4, 5].map(n => <i key={n} className={`bi bi-star ${n <= formData.vote ? '-fill' : ''} `} onClick={() => setFormData(n)}></i>)}
+                            <div className="rating mb-3 text-warning mt-3">
+                                {[1, 2, 3, 4, 5].map(n => <i key={n} className={`bi bi-star ${n <= formData.vote ? '-fill' : ''} `} onClick={setFormData}></i>)}
                             </div>
+
+                            <button type="submit" className="btn btn-dark">Invia</button>
                         </form>
                     </div>
                 </div>
