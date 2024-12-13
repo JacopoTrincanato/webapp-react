2deccc2a8a2660d274e119b865a363671da0d1f2
JacopoTrincanato <trincanatoj@gmail.com>
2024-12-13 16:49:11 +0100

    create const formData and setFormData and create function handleFormData

--- src/components/ReviewForm.jsx
+++ src/components/ReviewForm.jsx
@@ -4,11 +4,36 @@ import { useState, useEffect } from "react";
 //creo il componente ReviewForm
 export default function ReviewForm() {
 
+    //creo un oggetto con le proprietà delle review
+    const newReview = {
+        username: '',
+        review: '',
+        vote: 0
+    };
+
+    //creo una costante dove salvare i dati del form
+    const [formData, setFormData] = useState(newReview);
+
     //creo una costante dove salvare lo username
-    const [username, setUsername] = useState('');
+    //const [username, setUsername] = useState('');
 
     //creo una costante dove salvare la recensione
-    const [review, setReview] = useState('');
+    //const [review, setReview] = useState('');
+
+    //creo una costante dove aggiungere il proprio voto
+    //const [rating, setRating] = useState(0);
+
+    //creo la funzione handleFormData dove aggiornare i dati del form
+    function handleFormData(e) {
+        setFormData((formData) => ({
+            ...formData,
+            [e.target.name]: e.target.value,
+        }));
+    };
+
+    function handleFormSubmit(e) {
+        e.preventDefault()
+    }
 
     //eseguo il return
     return (
@@ -16,18 +41,23 @@ export default function ReviewForm() {
             <div className="container">
                 <div className="card">
                     <div className="card-body">
-                        <form>
+                        <form onSubmit={handleFormSubmit}>
 
                             {/*input per lo username */}
                             <div className="mb-3">
                                 <label htmlFor="username">Username</label>
-                                <input type="text" placeholder="Inserisci lo username" value={username} onChange={(e) => setUsername(e.target.value)} />
+                                <input type="text" placeholder="Inserisci lo username" value={formData.username} onChange={handleFormData} />
                             </div>
 
                             {/*input per la recensione */}
                             <div className="mb3">
                                 <label htmlFor="review">Review</label>
-                                <textarea name="review" id="review" placeholder="Inserisci la recensione" value={review} onChange={(e) => setReview(e.target.value)}></textarea>
+                                <textarea name="review" id="review" placeholder="Inserisci la recensione" value={formData.review} onChange={handleFormData}></textarea>
+                            </div>
+
+                            {/*imput per il voto*/}
+                            <div className="rating mb-3 text-warning">
+                                {[1, 2, 3, 4, 5].map(n => <i key={n} className={`bi bi-star ${n <= formData.vote ? '-fill' : ''} `} onClick={() => setFormData(n)}></i>)}
                             </div>
                         </form>
                     </div>
