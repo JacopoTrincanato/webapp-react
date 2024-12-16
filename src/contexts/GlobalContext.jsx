a352719b71f52d5b7fd0f71d35738c08f48d4756
JacopoTrincanato <trincanatoj@gmail.com>
2024-12-16 14:48:34 +0100

    create contexts folder and create GlobalContext.jsx

---
+++ src/contexts/GlobalContext.jsx
@@ -0,0 +1,8 @@
+//importo createContext
+import { createContext } from "react";
+
+//creo una variabile GlobalContext
+const GlobalContext = createContext();
+
+//esporto GlobalContext
+export default GlobalContext
